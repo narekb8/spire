627e81b677f330ed45a465e2cfc257be6cc29f0c
narekb8 <narekb8@gmail.com>
2025-02-04 00:55:24 -0500

    Updated gitignore and move Aren Test 1 to signature.c

--- prime/src/signature.c
+++ prime/src/signature.c
@@ -102,6 +102,29 @@ void SIG_Add_To_Pending_Messages(signed_message *m, int32u dest_bits,
   //if (dest_bits == BROADCAST)
   //  PROCESS_Message(m);
 
+  /* Move Aren's test cases to one place in prime for ease of use.
+   * Just run a switch statement across all the relevant packet types,
+   * letting us add more later more conveniently. */
+  switch ((enum packet_types) m->type)
+  {
+  case PO_ACK:
+    //Aren Test 1
+    po_ack_message *po_ack_specific = (po_ack_message*)(m + 1);
+    po_ack_part *ack_part = (po_ack_part *)(po_ack_specific + 1);
+    if (VAR.My_Server_ID == 1 && ack_part->seq.seq_num % 10 == 0) {
+    //printf("\nNo Change in the seq\n");
+        srand(time(NULL)); 
+        ack_part->seq.seq_num = rand();//%51;
+        ack_part->seq.incarnation = rand();
+    }
+    // small change ToDo change -->> ps.seq_num--; ps.seq_num;
+    //random change in sequence number    ps.seq_num = rand();
+    break;
+  
+  default:
+    break;
+  }
+
   if(UTIL_DLL_Is_Empty(&DATA.SIG.pending_messages_dll)) {
     UTIL_Stopwatch_Start(&DATA.SIG.max_batch_sw);
     E_queue(SIG_Make_Batch, BATCH_MAX_TRIGGER, NULL, DATA.SIG.sig_max_time);
